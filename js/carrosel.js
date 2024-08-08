1e4813483514da12d1f8e197bba3c7d4a2a85354
vinicius nogueira <v.francisco@riprisma.com>
2024-08-08 15:43:46 -0300

    second commit

---
+++ js/carrosel.js
@@ -0,0 +1,31 @@
+var swiper = new Swiper(".mySwiper-person", {
+  loop: false,
+  slidesPerView: "auto",
+  spaceBetween: 32,
+
+  //   autoplay: {
+  //     delay: 2500,
+  //     disableOnInteraction: false,
+  //   },
+
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+});
+
+var swiper = new Swiper(".mySwiper-agenda", {
+  loop: false,
+  slidesPerView: "auto",
+  spaceBetween: 32,
+
+  //   autoplay: {
+  //     delay: 2500,
+  //     disableOnInteraction: false,
+  //   },
+
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+});
